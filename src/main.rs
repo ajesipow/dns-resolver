a0d44fff5294e44de49d6f28ed9bc6d2daee8e06
ajesipow <alex>
2023-12-27 12:17:05 +0100

    Add clap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use crate::core::{resolve, TYPE_A};
+use clap::Parser;
 
 mod core;
 mod header;
@@ -6,16 +7,20 @@ mod packet;
 mod question;
 mod record;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    // The domain name to lookup
+    #[arg(short, long)]
+    name: String,
+
+    // The record type
+    #[arg(short, long, default_value_t = 1)]
+    record_type: u16,
+}
+
 fn main() {
-    let domains = [
-        "www.example.com",
-        "recurse.com",
-        "metafilter.com",
-        "www.facebook.com",
-        "www.metafilter.com",
-    ];
-    for domain in domains {
-        let response = resolve(domain, TYPE_A).unwrap();
-        println!("{} is at {}", domain, response);
-    }
+    let args = Args::parse();
+    let response = resolve(&args.name, args.record_type).unwrap();
+    println!("{} is at {}", args.name, response);
 }
